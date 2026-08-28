1dd3d2c43770bc49240bdb1ce46dabd536dba9aa
emmanuel <jhonsonola2026@gmail.com>
2026-08-28 12:23:05 -0400

    shut down.py

---
+++ shut down.py
@@ -0,0 +1,7 @@
+def shutting_down():
+    answer = input("do you want to shutdown device: ")
+    if answer.lower() == "yes":
+        print("shutting down")
+    else:
+        print("sorry")
+shutting_down()
