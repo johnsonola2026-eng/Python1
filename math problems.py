59856673ef7113f158306f5788be668b90b5deb8
johnsonola2026-eng <johnsonola2026@gmail.com>
2026-09-08 17:45:19 -0400

    trignometric value.py

---
+++ math problems.py
@@ -0,0 +1,3 @@
+import math
+print('the floor and ceiling value of 23.56 are:',+str(math.ceil(23.56))+','+
+str(math.floor(23.56)))
---
+++ trignometric value.py
@@ -0,0 +1,4 @@
+import math
+user_choice = input("enter a number to find its trignometric value:")
+sum = math.sin(float(user_choice))
+print(sum)
