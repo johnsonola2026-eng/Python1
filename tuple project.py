f6810737a7cd3d46005e466ad4450cb77f18766e
johnsonola2026-eng <johnsonola2026@gmail.com>
2026-09-15 18:05:21 -0400

    tuple project.py

---
+++ tuple project.py
@@ -0,0 +1,7 @@
+tup1=input(tuple("4,3,2,2,-1,18"))
+tup2=input(tuple("9,2,4,8,8,3,2,9"))
+if ("multiply all numbers in both tuples"):
+    sum==tup1*tup2
+    print(sum,tup1*tup2)
+else:
+ print("error")
