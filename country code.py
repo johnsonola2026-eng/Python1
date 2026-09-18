04c42e28decb4e33ada6800b14f15ddf08a08ea4
johnsonola2026-eng <johnsonola2026@gmail.com>
2026-09-18 19:46:38 -0400

    country code.py

---
+++ country code.py
@@ -0,0 +1,7 @@
+country_code={'india':'0091',
+              'australia':'0025',
+              'nepal':'00977'}
+print("the country code for india is-")
+print(country_code.get('india','not found'))
+print("the country code for japan is-")
+print(country_code.get('japan','not found'))
