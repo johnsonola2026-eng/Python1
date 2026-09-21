bed15759215afe52c50c1e04cee9733185e94d0c
johnsonola2026-eng <johnsonola2026@gmail.com>
2026-09-21 08:11:21 -0400

    list practice.py

---
+++ list practice.py
@@ -0,0 +1,9 @@
+x='odd'
+input(("would you like to print x y/n:"))
+list1=[1,3,5,7,9]
+print(list1)
+list2=[7,9,11,13,15]
+print(list2)
+list3=['apple','blueberry','cranberry','orange']
+list3=['Apple','Blueberry','Cranberry','Orange']
+print(list3)
